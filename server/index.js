4068f61645840cfba5b564e91b5d981e5277311f
jaiv shah <jaiv.maurya@gmail.com>
2021-05-15 12:19:14 -0400

    Solved Error in previos step step-2 is completed perfectly

--- server/index.js
+++ server/index.js
@@ -20,31 +20,67 @@ app.get("/api/ping", (req, res) => {
 
 // Step-2
 app.get("/api/posts", (req, res) => {
-  axios
-    .get("http://hatchways.io/api/assessment/blog/posts")
-    .then((response) => {
-      res.send(response.data);
-    });
+  // Gets all posts with at least one tag from the API using axios
   axios
     .all([
       axios.get("http://hatchways.io/api/assessment/blog/posts?tag=tech"),
       axios.get("http://hatchways.io/api/assessment/blog/posts?tag=history"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=health"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=startups"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=science"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=design"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=culture"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=politics"),
+      axios.get("http://hatchways.io/api/assessment/blog/posts?tag=science"),
     ])
     .then(
-      axios.spread((response1, response2) => {
-        const data1 = response1.data.posts;
-        const data2 = response2.data.posts;
-        const posts = data1.concat(data2);
-        const post = {
-          posts,
-        };
-        res.status(200).send(post);
-      })
+      axios.spread(
+        (
+          response1,
+          response2,
+          response3,
+          response4,
+          response5,
+          response6,
+          response7,
+          response8,
+          response9
+        ) => {
+          // Organizes data into an array
+          let data = [
+            response1.data.posts,
+            response2.data.posts,
+            response3.data.posts,
+            response4.data.posts,
+            response5.data.posts,
+            response6.data.posts,
+            response7.data.posts,
+            response8.data.posts,
+            response9.data.posts,
+          ];
+          // Object so that a hash can be made on the id of the post and remove duplicates
+          let post = {};
+          let posts = [];
+          for (let i = 0; i < data.length; i++) {
+            let blog = data[i];
+            for (let i = 0; i < blog.length; i++) {
+              post[blog[i].id] = blog[i];
+            }
+          }
+          // Create response object so that the result of the request is in the correct format
+          for (let key in post) {
+            posts.push(post[key]);
+          }
+          let response = {
+            posts,
+          };
+          res.status(200).send(response);
+        }
+      )
     )
     .catch((error) => {
-      console.log(error); // Just to debug error
       res.status(400).send({
-        error: "Tags is required",
+        error: "Tags parameter is required",
       });
       console.log(error);
     });
